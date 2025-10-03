21f456d76589bd447bf6c838c5bc3b1211cf1f9d
Sameer <sameerthecoolguy2006@gmail.com>
2025-10-03 23:14:25 +0500

    Hangman empty window

--- main.c
+++ main.c
@@ -1,8 +1,59 @@
 #include <SDL2/SDL.h>
+#include <glad/glad.h>
+#define SDL_MAIN_HANDLED
+#include <stdbool.h>
 #include "hangman.h"
 
 int main(int argc, char* argv[]) {
-    test();
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
 
+    if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
+        printf("Could not init SDL: %s\n", SDL_GetError());
+        exit(1);
+    }
+
+    SDL_Window *window = SDL_CreateWindow(
+        "Hangman",
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        800, 600,
+        SDL_WINDOW_OPENGL
+    );
+
+    if (!window) {
+        printf("Failed to init window: %s\n", SDL_GetError());
+        exit(1);
+    }
+
+    SDL_GL_CreateContext(window);
+    if (!gladLoadGLLoader((GLADloadproc)SDL_GL_GetProcAddress)) {
+        printf("Failed to load GL: %s\n", SDL_GetError());
+        exit(1);
+    }
+
+    puts("OpenGL loaded");
+    printf("Vendor:   %s\n", glGetString(GL_VENDOR));
+    printf("Renderer: %s\n", glGetString(GL_RENDERER));
+    printf("Version:  %s\n", glGetString(GL_VERSION));
+
+    bool shouldQuit = false;
+
+    while (!shouldQuit) {
+        SDL_Event event;
+
+        while (SDL_PollEvent(&event)) {
+            switch (event.type) {
+                case SDL_QUIT:
+                    shouldQuit = true;
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    test();
     return 0;
 }
--- readme.md
+++ readme.md
@@ -1,1 +1,5 @@
 # Hangman
+
+### Dependencies
+- SDL2
+- glad
